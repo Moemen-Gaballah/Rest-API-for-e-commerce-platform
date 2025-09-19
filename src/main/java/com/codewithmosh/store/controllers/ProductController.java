6382812612f6b3ebd8832d3efb62ff39d6314397
moemen gaballah <moemengaballa@gmail.com>
2025-09-19 08:12:57 +0300

    delete product

--- src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -76,4 +76,17 @@ public class ProductController {
         productDto.setId(product.getId());
         return ResponseEntity.ok(productDto);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        var product = productRepository.findById(id).orElse(null);
+
+        if(product == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        productRepository.delete(product);
+
+        return ResponseEntity.noContent().build();
+    }
 }
